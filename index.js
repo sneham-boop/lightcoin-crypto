23b6330b1f0172011236ce5eecd429acb372e8f5
Sneha Mahajan <sneh.km@gmail.com>
2022-04-11 17:02:26 -0400

    Code for LightCoin completed

--- index.js
+++ index.js
@@ -1,29 +1,84 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    let bal = 0;
+    for (const transaction of this.transactions) {
+      bal += transaction.value;
+    }
+    return bal;
+  }
+  addTransactions(transaction) {
+    this.transactions.push(transaction);
+  }
+}
 
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransactions(this);
+    console.log(
+      `${this.value > 0 ? "Deposit" : "Withdrawal"} of amount ${
+        this.value
+      } was succesful!!`
+    );
   }
 
-  commit() {
-    balance -= this.amount;
+  // Alternative method
+  // isAllowed() {
+  //   if (this.value > 0) {
+  //     // Deposit condition
+  //     return true;
+  //   }
+
+  //   // Check if we have enough money if withdrawing
+  //   const cashAvailable = this.account.balance + this.value;
+  //   if (cashAvailable >= 0) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed(){
+    // Do we have money?
+    return (this.account.balance + this.value >= 0);
   }
 
 }
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
 
-
+  isAllowed(){
+    return true;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+console.log("Starting balance: ", myAccount.balance);
+t1 = new Deposit(10000, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(3000, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+t3 = new Deposit(15000, myAccount);
+t3.commit();
+console.log("Ending alance:", myAccount.balance);
+console.log("Transactions on my account:", myAccount.transactions);
